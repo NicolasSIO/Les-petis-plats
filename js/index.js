97b8a605dad8521333a5863b841e00cd30eadce5
NicolasSIO <nicolas.mazaud.sio@gmail.com>
2023-12-14 17:57:26 +0100

    Add tags DOM

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 import { recipes } from "../data/recipes.js";
 import { createCard } from "./card.js";
+import { createAppliance, createIngredient, createUstensils } from "./tag.js";
 
 const title = document.querySelector(".title");
 const nbRecipes = document.querySelector(".recipes-nb");
@@ -9,7 +10,6 @@ const searchTags = document.querySelectorAll(".search-tags-title");
 
 searchTags.forEach((el) => {
   el.addEventListener("click", () => {
-    console.log(el);
     if (el.parentNode.childNodes[3].classList.contains("display-none")) {
       el.parentNode.childNodes[3].classList.remove("display-none");
       el.parentNode.childNodes[3].classList.add("display-flex");
@@ -38,9 +38,48 @@ const displayCard = (recipes) => {
   });
 };
 
+const displayTags = (recipes) => {
+  let ingredients = [];
+  let appliances = [];
+  let ustensils = [];
+  let ingredientContainer = document.querySelector(".ingredients");
+  let applianceContainer = document.querySelector(".appliances");
+  let ustensilContainer = document.querySelector(".ustensils");
+
+  recipes.forEach((recipe) => {
+    recipe.ingredients.forEach((ingredient) => {
+      if (!ingredients.includes(ingredient.ingredient)) {
+        ingredients.push(ingredient.ingredient);
+      }
+    });
+    if (!appliances.includes(recipe.appliance)) {
+      appliances.push(recipe.appliance);
+    }
+    recipe.ustensils.forEach((ustensil) => {
+      if (!ustensils.includes(ustensil)) {
+        ustensils.push(ustensil);
+      }
+    });
+  });
+  ingredients.forEach((ingredient) => {
+    const ingredientTag = createIngredient(ingredient);
+    ingredientContainer.innerHTML += ingredientTag;
+  });
+  appliances.forEach((appliance) => {
+    const applainceTag = createAppliance(appliance);
+    applianceContainer.innerHTML += applainceTag;
+  });
+  ustensils.forEach((ustensil) => {
+    const ustensilTag = createUstensils(ustensil);
+    console.log(ustensil);
+    ustensilContainer.innerHTML += ustensilTag;
+  });
+};
+
 const main = () => {
   try {
     displayCard(recipes);
+    displayTags(recipes);
   } catch (error) {
     console.error("Une erreur est survenue :", error);
   }
---
+++ js/tag.js
@@ -0,0 +1,22 @@
+export const createIngredient = (ingredient) => {
+  const ingredients = `
+      <li class="tag">${ingredient}</li>
+    `;
+
+  return ingredients;
+};
+
+export const createAppliance = (applaince) => {
+  const applainces = `
+      <li class="tag">${applaince}</li>
+    `;
+
+  return applainces;
+};
+export const createUstensils = (ustensil) => {
+  const ustensils = `
+      <li class="tag">${ustensil}</li>
+    `;
+
+  return ustensils;
+};
